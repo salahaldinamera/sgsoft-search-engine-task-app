dfd1eb06c4165306e9ca48d2e8f5cd316e93d979
Salah-Aldin Amera <60547056+salahaldinamera@users.noreply.github.com>
2022-02-13 00:52:55 +0200

    test-heroku-fix-2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://sgsoft-search-engine-task-api.herokuapp.com/',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
